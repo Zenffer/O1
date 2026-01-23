8c741855049826b5a7ef10bc4fb4fb6d884f81e2
Jasmin Ann Bobares <jabobares@my.cspc.edu.ph>
2026-01-23 09:29:26 +0800

    01.1

--- fileManagement.js
+++ fileManagement.js
@@ -1,12 +1,28 @@
+// const fs = require('fs');
+
+//     fs.readFile('log.txt','utf8', (err, data) =>{
+//         if (err) throw err;
+//         console.log(data);
+
+//         const updateData = data.replace('New Line','CSPC');
+
+//         fs.writeFile('log.txt', updateData, 'utf8', (err) => {
+//             if (err) throw err;
+//             console.log('File updated successfully.');
+//         });
+//     });
+/////////////////////////////
+
+// const fs = require('fs');
+//     fs.mkdir('uploads',err =>{
+//         if (err) throw err;
+//         console.log('Folder created successfully.');
+
+////////////////////////////
+
 const fs = require('fs');
-
-    fs.readFile('log.txt','utf8', (err, data) =>{
+    fs.unlink('sample.txt', err =>{
         if (err) throw err;
+        console.log('File deleted successfully.');
 
-        const updateData = data.replace('New Line','CSPC');
-
-        fs.writeFile('log.txt', updateData, 'utf8', (err) => {
-            if (err) throw err;
-            console.log('File updated successfully.');
-        });
-    });
+});
