9505938ac8fbe4d0800bc25c6ac562a4b97eeb1a
Jasmin Ann Bobares <jabobares@my.cspc.edu.ph>
2026-01-23 09:36:18 +0800

    1.2

--- fileManagement.js
+++ fileManagement.js
@@ -20,9 +20,17 @@
 
 ////////////////////////////
 
-const fs = require('fs');
-    fs.unlink('sample.txt', err =>{
-        if (err) throw err;
-        console.log('File deleted successfully.');
+// const fs = require('fs');
+//     fs.unlink('sample.txt', err =>{
+//         if (err) throw err;
+//         console.log('File deleted successfully.');
 
-});
+const os = require('os');
+console.log('Operating System Info:');
+console.log(`Platform: ${os.platform()}`);
+console.log(`Architecture: ${os.arch()}`);
+console.log(`CPU Cores: ${os.cpus().length}`);
+console.log(`Total Memory: ${(os.totalmem() / (1024 ** 3)).toFixed(2)} GB`);
+console.log(`Free Memory: ${(os.freemem() / (1024 ** 3)).toFixed(2)} GB`);
+    
+// });
