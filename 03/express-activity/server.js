5823e8d8fe216064c22161ed0d2f3b7ff00fcda9
Jasmin Ann Bobares <jabobares@my.cspc.edu.ph>
2026-02-20 09:15:38 +0800

    egth

--- 03/express-activity/server.js
+++ 03/express-activity/server.js
@@ -4,9 +4,25 @@ const app = express();
 const hostnamae = 'localhost';
 const port = 3000;
 
+// app.get('/', (req, res) => {
+//     res.send('<h1>Welcome to the Home Page, MF</h1>');
+// });
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to the Home Page</h1>');
+    res.send('Get request from the home page');
 });
+
+app.post('/', (req, res) => {
+    res.send('Post request from to the home page');
+});
+
+// app.put('/', (req, res) => {
+//     res.send('Put request from to the home page');
+// });
+
+// app.delete('/', (req, res) => {
+//     res.send('Delete request from to the home page');
+// });
+
 
 app.get('/about', (req, res) => {
     res.send('<h1>About Page</h1><p>This is the about page.</p>');
