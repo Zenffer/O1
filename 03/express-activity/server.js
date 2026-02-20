34a000db159bc7326526d31021fe1b382ca7ba19
Jasmin Ann Bobares <jabobares@my.cspc.edu.ph>
2026-02-20 10:04:48 +0800

    ffstrdyfug

--- 03/express-activity/server.js
+++ 03/express-activity/server.js
@@ -23,11 +23,25 @@ app.post('/', (req, res) => {
 //     res.send('Delete request from to the home page');
 // });
 
-
+// Parameterized route
 app.get('/about', (req, res) => {
     res.send('<h1>About Page</h1><p>This is the about page.</p>');
+});
+
+app.get('user/:userID/name/:userName', (req, res) => {
+    res.send(`<h1>User Info</h1><p>User ID: ${req.params.userID}, User Name: ${req.params.userName}</p>`);
+});
+
+app.get('/search', (req, res) => {
+    res.send(`ID: ${req.query.userID},
+        Name: ${req.query.name}`);
 });
 
 app.listen(port, hostnamae, () => {
     console.log(`Server is running on http://${hostnamae}:${port}`);
 });
+
+// app.use((req, res) => {
+//     res.status(404).send('<h1>404 Not Found</h1><p>The page you are looking for does not exist.</p>');
+// });
+
