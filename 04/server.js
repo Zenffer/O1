c1a5a62e7a79d431fc1991225e9a7aeefd10b1e8
Jasmin Ann Bobares <jabobares@my.cspc.edu.ph>
2026-03-06 09:08:16 +0800

    sdfgh

--- 04/server.js
+++ 04/server.js
@@ -23,18 +23,23 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON bodies
 
 // Route to handle POST requests to /data
-app.post('/data', (req, res) => { 
+app.post('/data', (req, res) => {
   console.log('Received data:', req.body); // Log the received data to the console
   res.send('Data received successfully!');
 });
 
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
-app.post('/submit', (req, res) => {
+app.use(express.static('public')); // Middleware to serve static files from the 'public' directory
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html'); // Serve the login.html file when the root URL is accessed
+});
+
+app.post('/login', (req, res) => {
   console.log('Received form data:', req.body); // Log the received form data to the console
   res.send('Form data received successfully!');
 });
-
 
 app.listen(3000, () => {
   console.log('Server running on port http://localhost:3000');
