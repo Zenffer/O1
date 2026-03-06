cb947b8abfe7372aeca6030c74c04de590ee1f16
Jasmin Ann Bobares <jabobares@my.cspc.edu.ph>
2026-03-06 08:39:19 +0800

    xfg

--- 04/server.js
+++ 04/server.js
@@ -1,23 +1,33 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 
-// First middleware
-app.use((req, res, next) => {
-  console.log('Middleware 1: This always runs');
-  next();
+// // First middleware
+// app.use((req, res, next) => {
+//   console.log('Middleware 1: This always runs');
+//   next();
+// });
+
+// // Second middleware
+// app.use((req, res, next) => {
+//   console.log('Middleware 2: This also always runs');
+//   next();
+// });
+
+// // Route handler
+// app.get('/', (req, res) => {
+//   console.log('Route handler: This runs when the root URL is accessed');
+//   res.send('Hello World!');
+// });
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+// Route to handle POST requests to /data
+app.post('/data', (req, res) => { 
+  console.log('Received data:', req.body); // Log the received data to the console
+  res.send('Data received successfully!');
 });
 
-// Second middleware
-app.use((req, res, next) => {
-  console.log('Middleware 2: This also always runs');
-  next();
-});
-
-// Route handler
-app.get('/', (req, res) => {
-  console.log('Route handler: This runs when the root URL is accessed');
-  res.send('Hello World!');
-});
 
 app.listen(3000, () => {
   console.log('Server running on port http://localhost:3000');
